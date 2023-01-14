aefb8b363a432967f550a2a252ee306dfec87570
Shinya-Fukuta0116 <alleyezon.djwanted@gmail.com>
2023-01-15 08:46:02 +0900

    add function of filter button

--- app.js
+++ app.js
@@ -1,4 +1,5 @@
 const fileInput = document.querySelector('.file-input'),
+  filterOptions = document.querySelectorAll('.filter button'),
   previewImg = document.querySelector('.preview-img img'),
   chooseImgBtn = document.querySelector('.choose-img');
 
@@ -6,7 +7,17 @@ const loadImage = () => {
   let file = fileInput.files[0];
   if (!file) return;
   previewImg.src = URL.createObjectURL(file);
+  previewImg.addEventListener('load', () => {
+    document.querySelector('.container').classList.remove('disable');
+  })
 }
+
+filterOptions.forEach(option => {
+  option.addEventListener('click', () => {
+    document.querySelector('.filter .active').classList.remove('active');
+    option.classList.add('active');
+  })
+})
 
 fileInput.addEventListener('change', loadImage);
 chooseImgBtn.addEventListener('click', () => fileInput.click());
